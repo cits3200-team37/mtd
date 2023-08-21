9b458e51ac5fcecfb867683f81e561be14b8a630
nicodeas <nicholas.hy.michlin@gmail.com>
2023-08-21 17:08:48 +0800

    basic simulation endpoint

--- server/server/app.py
+++ server/server/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from experiments.run import simulate_without_saving
+import networkx as nx
 
 app = Flask(__name__)
 
@@ -6,3 +8,27 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/simulate", methods=["GET"])
+def simulate():
+    finish_time = request.json.get("finishTime")
+    mtd_interval = request.json.get("mtdInterval")
+    scheme = request.json.get("scheme")
+    total_nodes = request.json.get("totalNodes")
+    if not all([finish_time, mtd_interval, scheme, total_nodes]):
+        return {}, 400
+
+    result = simulate_without_saving(
+        start_time=0,
+        finish_time=finish_time,
+        mtd_interval=mtd_interval,
+        scheme=scheme,
+        total_nodes=total_nodes,
+    )
+
+    resulting_graph = nx.node_link_data(result.get_network().graph)
+    for node in resulting_graph["nodes"]:
+        node["host"] = node["host"].to_json()
+
+    return resulting_graph, 200
