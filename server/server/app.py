ea12fcb7876e0d491887114d4d5afb3be05190b7
nicodeas <nicholas.hy.michlin@gmail.com>
2023-08-21 23:05:32 +0800

    api CORS

--- server/server/app.py
+++ server/server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 from experiments.run import simulate_without_saving
 import networkx as nx
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
