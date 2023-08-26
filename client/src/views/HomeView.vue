444137e7a5f21bc622e817e6147f6437f52d0cc8
crystalycteh <crystalhellocrystal@gmail.com>
2023-08-26 16:09:07 +0800

    Adjust colours and links

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-gray-100">
+  <div class="flex items-center justify-center h-screen bg-dark-background text-white">
     <div class="text-center">
       <h1 class="text-3xl font-semibold mb-2 px-20">
         Welcome to the Moving Target Defence Simulator
       </h1>
-      <p class="text-gray-600">
+      <p class="font-light text-navbar-icon">
         Integrate time domain in the evaluation of Moving Target Defence
       </p>
 
@@ -12,14 +12,14 @@
         class="flex justify-center items-center mt-3 space-x-20 max-w-full mx-auto"
       >
         <a
-          href="https://www.https://github.com/MoeBuTa/MTDSimTime.com"
+          href="https://github.com/cits3200-team37"
           target="_blank"
-          class="flex flex-col items-center mt-10 text-gray-500"
+          class="flex flex-col items-center mt-10 text-navbar-icon"
         >
           <div class="h-12 w-12">
             <svg
               viewBox="0 0 24 24"
-              class="fill-current text-gray-500 hover:text-blue-500"
+              class="fill-current text-navbar-icon hover:text-white"
             >
               <path :d="mdiGithub"></path>
             </svg>
@@ -30,12 +30,12 @@
         <a
           href="https://research-repository.uwa.edu.au/en/persons/jin-hong"
           target="_blank"
-          class="flex flex-col items-center mt-10 text-gray-500"
+          class="flex flex-col items-center mt-10 text-navbar-icon"
         >
           <div class="h-12 w-12">
             <svg
               viewBox="0 0 24 24"
-              class="fill-current text-gray-500 hover:text-blue-500"
+              class="fill-current text-navbar-icon hover:text-white"
             >
               <path :d="mdiFileDocument"></path>
             </svg>
@@ -44,14 +44,13 @@
         </a>
 
         <a
-          href="https://www.google.com"
-          target="_blank"
-          class="flex flex-col items-center mt-10 text-gray-500"
+          @click="toSimulateView()"
+          class="flex flex-col items-center mt-10 text-navbar-icon"
         >
           <div class="h-12 w-12">
             <svg
               viewBox="0 0 24 24"
-              class="fill-current text-gray-500 hover:text-blue-500"
+              class="fill-current text-navbar-icon hover:text-white"
             >
               <path :d="mdiGraphOutline"></path>
             </svg>
@@ -67,4 +66,11 @@
 import { mdiGithub } from "@mdi/js";
 import { mdiFileDocument } from "@mdi/js";
 import { mdiGraphOutline } from "@mdi/js";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const toSimulateView = async () => {
+  await router.push("/simulation");
+};
 </script>
