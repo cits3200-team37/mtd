dd2a5a875ce22ebb7a0dbd276a80c4e051788eea
benvarian <88700858+benvarian@users.noreply.github.com>
2023-08-27 13:53:47 +0800

    MTDSIM-22 remove unused components

--- client/src/components/SimView/D3Chart.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <!-- TODO: This doesn't grow to fill the available space -->
-  <div class="canvas-container border-black border-100 px-6 py-4">
-    <div class="text-center text-black">Network Graph:</div>
-    <img id="d3-image" src="" alt="D3 Visualization" />
-  </div>
-</template>
-
-<script setup></script>
