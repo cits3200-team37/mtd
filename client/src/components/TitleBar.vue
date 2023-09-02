57c9891a027f560b49a137c5ea5e76a8116da8f9
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-02 17:40:39 +0800

    MTDSIM-46 format

--- client/src/components/TitleBar.vue
+++ client/src/components/TitleBar.vue
@@ -1,39 +1,45 @@
 <template>
-    <div class="drag w-screen inset-x-0 top-0 bg-navbar-primary h-9 pl-20 p-1.5 ">
-        <div class="flex flex-row items-center justify-center">
-            <button class="no-drag hover:bg-gray-500 hover:opacity-70 rounded-md px-2" @click="handleBack">
-                <svg-icon type="mdi" color="white" size="22" :path="mdiArrowLeft" />
-            </button>
-            <button class="no-drag hover:bg-gray-500 hover:opacity-70 rounded-md px-2" @click="handleForward">
-                <svg-icon type="mdi" color="white" size="22" :path="mdiArrowRight" />
-            </button>
-            <div class="no-drag rounded-md bg-dark-background w-1/3 text-center">
-                <p>{{ router.currentRoute.value.name }}</p>
-            </div>
-        </div>
+  <div class="drag w-screen inset-x-0 top-0 bg-navbar-primary h-9 pl-20 p-1.5">
+    <div class="flex flex-row items-center justify-center">
+      <button
+        class="no-drag hover:bg-gray-500 hover:opacity-70 rounded-md px-2"
+        @click="handleBack"
+      >
+        <svg-icon type="mdi" color="white" size="22" :path="mdiArrowLeft" />
+      </button>
+      <button
+        class="no-drag hover:bg-gray-500 hover:opacity-70 rounded-md px-2"
+        @click="handleForward"
+      >
+        <svg-icon type="mdi" color="white" size="22" :path="mdiArrowRight" />
+      </button>
+      <div class="no-drag rounded-md bg-dark-background w-1/3 text-center">
+        <p>{{ router.currentRoute.value.name }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import svgIcon from '@jamescoyle/vue-icon';
-import { mdiArrowLeft } from '@mdi/js';
-import { mdiArrowRight } from '@mdi/js';
-import router from "../router"
+import svgIcon from "@jamescoyle/vue-icon";
+import { mdiArrowLeft } from "@mdi/js";
+import { mdiArrowRight } from "@mdi/js";
+import router from "../router";
 
 const handleBack = async () => {
-    router.back();
-}
+  router.back();
+};
 const handleForward = async () => {
-    router.forward();
-}
+  router.forward();
+};
 </script>
 
 <style scoped>
 .drag {
-    -webkit-app-region: drag;
+  -webkit-app-region: drag;
 }
 
 .no-drag {
-    -webkit-app-region: no-drag;
+  -webkit-app-region: no-drag;
 }
 </style>
