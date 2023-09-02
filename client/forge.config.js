d8f474c4310db4e2143a42fc666bad22566be320
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-02 18:06:21 +0800

    MTDSIM-43 implement github action that might work, change forge config to include publisher, update app version from 1.0.0 to 0.0.1

--- client/forge.config.js
+++ client/forge.config.js
@@ -45,4 +45,16 @@ module.exports = {
       },
     },
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "cits3200-team37",
+          name: "mtd",
+        },
+        prerelease: true,
+      },
+    },
+  ]
 };
