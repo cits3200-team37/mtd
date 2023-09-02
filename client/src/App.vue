9c56541645924d355f2e23f8f5276c4fb4f52e00
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-02 19:33:11 +0800

    MTDSIM-46 add back in the titlebar after merge conflict was done wrong

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,7 @@
 import NavBar from "./components/NavBar.vue";
 import { RouterView } from "vue-router";
 import { onMounted } from "vue";
+import TitleBar from "./components/TitleBar.vue";
 // ! needs to be uncommented out for prod
 // import router from "./router";
 onMounted(async () => {
