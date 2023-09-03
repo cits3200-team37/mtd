b6077937dd540ee9db306779578d76317f8343fb
Xavier Picart <havi.picart@gmail.com>
2023-09-03 22:09:37 +0800

    prettier

--- client/src/views/ChartView.vue
+++ client/src/views/ChartView.vue
@@ -1,23 +1,21 @@
 <template>
   <div>
-    <h1>Chart page</h1>
+    <h1>Chart page</h1>      
     <div v-if="data != null">
       Updated
       <AbyHost :attackRecord="data" />
     </div>
-    <div v-else>
-      null
-    </div>
+    <div v-else>null</div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import { useChartStore } from '../stores/charts';
-import AbyHost from '../components/AbyHost.vue';
+import { onMounted, ref } from "vue";
+import { useChartStore } from "../stores/charts";
+import AbyHost from "../components/AbyHost.vue";
 
 const store = useChartStore();
-const data = ref()
+const data = ref();
 
 onMounted(async () => {
   try {
