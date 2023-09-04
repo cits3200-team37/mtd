b8b4679b75e5ad9f34eea7a1be81080629a91386
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-04 11:58:58 +0800

    tooltip component

---
+++ client/src/components/ToolTip.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="absolute bg-gray-700 text-center rounded-lg py-1 px-4 opacity-90">
+    <ul>
+      <li>IP: {{ ip }}</li>
+      <li>OS: {{ `${osType} ${osVersion}` }}</li>
+      <li>{{ `${totalUsers} ${totalUsers == 1 ? "User" : "Users"}` }}</li>
+      <li>
+        {{ `${totalServices} ${totalServices == 1 ? "Service" : "Services"}` }}
+      </li>
+      <li v-if="compromised" class="bg-red-600 rounded-full">Compromised</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  ip: String,
+  osType: String,
+  osVersion: String,
+  totalUsers: Number,
+  totalServices: Number,
+  compromised: Boolean,
+});
+</script>
