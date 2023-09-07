65e7ed14306cccc859a6e82b0318bb80f838cf61
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-07 20:14:25 +0800

    MTDSIM-50 many changes to themes, remove blue theme, finalise both dark and white

--- client/src/views/SettingsView.vue
+++ client/src/views/SettingsView.vue
@@ -4,7 +4,6 @@
     <h2>Theme Variants:</h2>
     <button @click="setTheme('dark')">Dark Theme</button>
     <button @click="setTheme('light')">Light Theme</button>
-    <button @click="setTheme('blue')">Blue Theme</button>
   </div>
 </template>
 
