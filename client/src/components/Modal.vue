c1a17e4b56631755e0a8322c8505bd64230e03d8
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-10 12:42:32 +0800

    basic modal poc

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,109 @@
+<template>
+  <div
+    @click="emit('close')"
+    class="flex justify-center items-center absolute inset-0 z-10 w-full h-full bg-black bg-opacity-70 transition"
+  >
+    <div
+      @click.stop
+      class="h-1/2 w-1/2 bg-dark-background border border-none rounded-md"
+    >
+      <div class="h-full w-full" id="service-graph"></div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { onMounted } from "vue";
+import * as d3 from "d3";
+const props = defineProps(["serviceGraph"]);
+const emit = defineEmits(["close"]);
+
+const plotNetwork = (graphData) => {
+  console.log(graphData);
+  const { width, height } = d3
+    .select("#service-graph")
+    .node()
+    .getBoundingClientRect();
+
+  // Set the position attributes of links and nodes each time the simulation ticks.
+  const ticked = () => {
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
+
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+  };
+  // Reheat the simulation when drag starts, and fix the subject position.
+  const dragstarted = (event) => {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  };
+
+  // Update the subject (dragged node) position during drag.
+  const dragged = (event) => {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  };
+
+  // Restore the target alpha so the simulation cools after dragging ends.
+  // Unfix the subject position now that it’s no longer being dragged.
+  const dragended = (event) => {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+  };
+
+  // setup graph
+  const links = graphData.links.map((d) => ({ ...d }));
+  const nodes = graphData.nodes.map((d) => ({ ...d }));
+
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force(
+      "link",
+      d3.forceLink(links).id((d) => d.id),
+    )
+    .force("charge", d3.forceManyBody().strength(-200))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", ticked);
+
+  const svg = d3
+    .select("#service-graph")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .append("g");
+
+  const link = svg
+    .append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll()
+    .data(links)
+    .join("line")
+    .attr("stroke-width", (d) => Math.sqrt(d.value));
+
+  const node = svg
+    .append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+    .selectAll()
+    .data(nodes)
+    .join("circle")
+    .attr("class", "node")
+    .attr("r", 8)
+    .style("cursor", "pointer");
+
+  node.append("title").text((d) => d.id);
+  node.call(
+    d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended),
+  );
+};
+
+onMounted(() => {
+  plotNetwork(props.serviceGraph);
+});
+</script>
