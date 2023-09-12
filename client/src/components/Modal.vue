b5bfb9657fb450885c0e79cdd09a7027d8c0d30e
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-12 10:41:43 +0800

    modal component

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -1,114 +1,38 @@
 <template>
-  <div
-    @click="emit('close')"
-    class="flex justify-center items-center absolute inset-0 z-10 w-full h-full bg-black bg-opacity-40 drop-shadow-lg"
+  <transition
+    enter-from-class="opacity-0"
+    leave-to-class="opacity-0"
+    enter-active-class="transition duration-200"
+    leave-active-class="transition duration-200"
   >
     <div
-      @click.stop
-      class="h-1/2 w-1/2 bg-dark-background border border-none rounded-md"
+      v-if="showModal"
+      @click="emit('close')"
+      class="flex justify-center items-center absolute inset-0 z-10 w-screen h-screen bg-black bg-opacity-40 drop-shadow-lg"
     >
-      <div class="h-full w-full" id="service-graph"></div>
+      <div
+        @click.stop
+        class="p-2 container bg-dark-background border border-none rounded-md"
+      >
+        <div class="h-6 flex flex-row-reverse">
+          <svg-icon
+            @click="emit('close')"
+            type="mdi"
+            size="30"
+            class="text-navbar-icon hover:text-white hover:cursor-pointer transition"
+            :path="mdiWindowClose"
+          ></svg-icon>
+        </div>
+        <slot />
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 <script setup>
-import { onMounted } from "vue";
-import * as d3 from "d3";
-const props = defineProps(["serviceGraph", "compromisedServiceIds"]);
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiWindowClose } from "@mdi/js";
 const emit = defineEmits(["close"]);
-
-const plotNetwork = (graphData) => {
-  const { width, height } = d3
-    .select("#service-graph")
-    .node()
-    .getBoundingClientRect();
-
-  // Set the position attributes of links and nodes each time the simulation ticks.
-  const ticked = () => {
-    link
-      .attr("x1", (d) => d.source.x)
-      .attr("y1", (d) => d.source.y)
-      .attr("x2", (d) => d.target.x)
-      .attr("y2", (d) => d.target.y);
-
-    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-  };
-  // Reheat the simulation when drag starts, and fix the subject position.
-  const dragstarted = (event) => {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    event.subject.fx = event.subject.x;
-    event.subject.fy = event.subject.y;
-  };
-
-  // Update the subject (dragged node) position during drag.
-  const dragged = (event) => {
-    event.subject.fx = event.x;
-    event.subject.fy = event.y;
-  };
-
-  // Restore the target alpha so the simulation cools after dragging ends.
-  // Unfix the subject position now that it’s no longer being dragged.
-  const dragended = (event) => {
-    if (!event.active) simulation.alphaTarget(0);
-    event.subject.fx = null;
-    event.subject.fy = null;
-  };
-
-  // setup graph
-  const links = graphData.links.map((d) => ({ ...d }));
-  const nodes = graphData.nodes.map((d) => ({ ...d }));
-
-  const simulation = d3
-    .forceSimulation(nodes)
-    .force(
-      "link",
-      d3.forceLink(links).id((d) => d.id),
-    )
-    .force("charge", d3.forceManyBody().strength(-200))
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .on("tick", ticked);
-
-  const svg = d3
-    .select("#service-graph")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [0, 0, width, height])
-    .append("g");
-
-  const link = svg
-    .append("g")
-    .attr("stroke", "#999")
-    .attr("stroke-opacity", 0.6)
-    .selectAll()
-    .data(links)
-    .join("line")
-    .attr("stroke-width", (d) => Math.sqrt(d.value));
-
-  const node = svg
-    .append("g")
-    .attr("stroke", "#fff")
-    .attr("stroke-width", 1.5)
-    .selectAll()
-    .data(nodes)
-    .join("circle")
-    .attr("class", "node")
-    .attr("r", 8)
-    .style("cursor", "pointer")
-    .attr("fill", (d) => {
-      if (d.id in props.compromisedServiceIds) {
-        return "#FF0000";
-      }
-      return "#1f77b4";
-    });
-
-  node.append("title").text((d) => d.id);
-  node.call(
-    d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended),
-  );
-};
-
-onMounted(() => {
-  plotNetwork(props.serviceGraph);
+const props = defineProps({
+  showModal: Boolean,
 });
 </script>
