29c19d0bb0769d906636531b100d29d79f65c84a
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-13 11:12:40 +0800

    Revert "Resolve conflixts"
    This reverts commit 9999d965c8f6fd5032a25376339dc6dcdf172c51.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,42 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import NavBar from "./components/NavBar.vue";
 import { RouterView } from "vue-router";
 import DropDown from "./components/DropDown.vue";
-import { ref } from "vue";
+import TitleBar from "./components/TitleBar.vue";
 
-// const selectedOption = ref('Value');
+const theme = ref(null);
+const selectedOption = ref('Value');
 
-// const handleOptionSelected = (option) => {
-//   selectedOption.value = option;
-// };
+const handleOptionSelected = (option) => {
+  selectedOption.value = option;
+};
 
+onMounted(async () => {
+  try {
+    theme.value = localStorage.getItem("user-theme");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("mounted");
+  // await router.push("/");
+});
 
+window.addEventListener("theme-changed", (e) => {
+  theme.value = e.detail.storage;
+});
 </script>
 
 <template>
-  <NavBar />
-  <RouterView />
-  <DropDown heading="Menu" :menuOptions="['Option 1', 'Option 2', 'Option 3']" @emitOption="handleOptionSelected" />
-  <!-- <p class="text-white">{{ selectedOption }}</p>  -->
+  <div class="bg-background-color text-text-color" :class="{
+    'theme-dark': theme == 'dark',
+    'theme-light': theme == 'light',
+    'theme-blue': theme == 'blue',
+  }">
+    <TitleBar />
+    <NavBar />
+    <RouterView />
+    <DropDown heading="Menu" :menuOptions="['Option 1', 'Option 2', 'Option 3']" @emitOption="handleOptionSelected" />
+    <!-- <p class="text-white">{{ selectedOption }}</p> -->
+  </div>
 </template>
