313985db5e4d3d051633bdfb124b5bc825dceb0a
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-13 11:12:44 +0800

    Revert "Resolve conflicts"
    This reverts commit e505b4af187b87b7e9aae868ab15e5c4c0508457.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
-import { ref, onMounted } from "vue";
 import NavBar from "./components/NavBar.vue";
 import { RouterView } from "vue-router";
 import DropDown from "./components/DropDown.vue";
+import { ref } from "vue";
+
+// const selectedOption = ref('Value');
+
+// const handleOptionSelected = (option) => {
+//   selectedOption.value = option;
+// };
+
+import { onMounted } from "vue";
 import TitleBar from "./components/TitleBar.vue";
-
 const theme = ref(null);
-const selectedOption = ref('Value');
-
-const handleOptionSelected = (option) => {
-  selectedOption.value = option;
-};
-
+// ! needs to be uncommented out for prod
+// import router from "./router";
 onMounted(async () => {
   try {
     theme.value = localStorage.getItem("user-theme");
@@ -21,22 +24,23 @@ onMounted(async () => {
   console.log("mounted");
   // await router.push("/");
 });
-
-window.addEventListener("theme-changed", (e) => {
-  theme.value = e.detail.storage;
-});
 </script>
 
 <template>
-  <div class="bg-background-color text-text-color" :class="{
-    'theme-dark': theme == 'dark',
-    'theme-light': theme == 'light',
-    'theme-blue': theme == 'blue',
-  }">
+  <NavBar />
+  <RouterView />
+  <DropDown heading="Menu" :menuOptions="['Option 1', 'Option 2', 'Option 3']" @emitOption="handleOptionSelected" />
+  <!-- <p class="text-white">{{ selectedOption }}</p>  -->
+  <div
+    class="bg-background-color text-text-color"
+    :class="{
+      'theme-dark': theme == 'dark',
+      'theme-light': theme == 'light',
+      'theme-blue': theme == 'blue',
+    }"
+  >
     <TitleBar />
     <NavBar />
     <RouterView />
-    <DropDown heading="Menu" :menuOptions="['Option 1', 'Option 2', 'Option 3']" @emitOption="handleOptionSelected" />
-    <!-- <p class="text-white">{{ selectedOption }}</p> -->
   </div>
 </template>
