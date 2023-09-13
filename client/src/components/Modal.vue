6940b27d2ba32690cf2254f9f596af4f1c559292
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-13 09:09:25 +0800

    update modal

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -1,38 +1,31 @@
 <template>
-  <transition
-    enter-from-class="opacity-0"
-    leave-to-class="opacity-0"
-    enter-active-class="transition duration-200"
-    leave-active-class="transition duration-200"
+  <div
+    @click="emit('close')"
+    class="flex justify-center items-center absolute inset-0 z-10 w-screen h-screen bg-black bg-opacity-40 drop-shadow-lg"
   >
     <div
-      v-if="showModal"
-      @click="emit('close')"
-      class="flex justify-center items-center absolute inset-0 z-10 w-screen h-screen bg-black bg-opacity-40 drop-shadow-lg"
+      @click.stop
+      class="p-2 container bg-dark-background border border-none rounded-md"
     >
-      <div
-        @click.stop
-        class="p-2 container bg-dark-background border border-none rounded-md"
-      >
-        <div class="h-6 flex flex-row-reverse">
-          <svg-icon
-            @click="emit('close')"
-            type="mdi"
-            size="30"
-            class="text-navbar-icon hover:text-white hover:cursor-pointer transition"
-            :path="mdiWindowClose"
-          ></svg-icon>
-        </div>
-        <slot />
+      <div class="h-6 flex flex-row-reverse">
+        <svg-icon
+          @click="emit('close')"
+          type="mdi"
+          size="30"
+          class="text-navbar-icon hover:text-white hover:cursor-pointer transition"
+          :path="mdiWindowClose"
+        ></svg-icon>
       </div>
+      <slot />
     </div>
-  </transition>
+  </div>
 </template>
 <script setup>
 import SvgIcon from "@jamescoyle/vue-icon";
 import { mdiWindowClose } from "@mdi/js";
-const emit = defineEmits(["close"]);
-const props = defineProps({
-  showModal: Boolean,
+import { onMounted } from "vue";
+const emit = defineEmits(["close", "mounted"]);
+onMounted(() => {
+  emit("mounted");
 });
 </script>
