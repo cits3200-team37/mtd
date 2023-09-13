47cda2019d16b81a27b855c125c63f20559d1f85
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2023-09-13 10:39:40 +0800

    resolve merge conflicts

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,6 +9,21 @@ import { ref } from "vue";
 // const handleOptionSelected = (option) => {
 //   selectedOption.value = option;
 // };
+
+import { onMounted } from "vue";
+import TitleBar from "./components/TitleBar.vue";
+const theme = ref(null);
+// ! needs to be uncommented out for prod
+// import router from "./router";
+onMounted(async () => {
+  try {
+    theme.value = localStorage.getItem("user-theme");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("mounted");
+  // await router.push("/");
+});
 </script>
 
 <template>
@@ -16,4 +31,16 @@ import { ref } from "vue";
   <RouterView />
   <DropDown heading="Menu" :menuOptions="['Option 1', 'Option 2', 'Option 3']" @emitOption="handleOptionSelected" />
   <!-- <p class="text-white">{{ selectedOption }}</p>  -->
+  <div
+    class="bg-background-color text-text-color"
+    :class="{
+      'theme-dark': theme == 'dark',
+      'theme-light': theme == 'light',
+      'theme-blue': theme == 'blue',
+    }"
+  >
+    <TitleBar />
+    <NavBar />
+    <RouterView />
+  </div>
 </template>
