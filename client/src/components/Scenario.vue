74cbef7e4599b734b2e170e686ab88b01bf079d4
FearMyCode <mcalyip@gmail.com>
2023-09-17 14:51:42 +0800

    MTDSIM-64 Add border and hover background colour

--- client/src/components/Scenario.vue
+++ client/src/components/Scenario.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-20 p-4 bg-black text-white cursor-pointer" @click="useScenario">
+  <div class="h-20 p-4 border border-solid rounded-lg hover:bg-gray-400 hover:cursor-pointer" @click="useScenario">
     {{ scenarioMessage }}
   </div>
 </template>
