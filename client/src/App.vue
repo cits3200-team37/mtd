d9c77add83e93cae79d8b7ef0c28e79daac65825
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-27 12:55:13 +0800

    MTDSIM-43 testing

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,17 +4,17 @@ import NavBar from "./components/NavBar.vue";
 import { RouterView } from "vue-router";
 import { onMounted } from "vue";
 import TitleBar from "./components/TitleBar.vue";
+import router from " ./router";
 const theme = ref(null);
 // ! needs to be uncommented out for prod
-// import router from "./router";
 onMounted(async () => {
   try {
+    await router.push("/");
     theme.value = localStorage.getItem("user-theme");
   } catch (error) {
     console.log(error);
   }
   console.log("mounted");
-  // await router.push("/");
 });
 
 window.addEventListener("theme-changed", (e) => {
@@ -22,14 +22,11 @@ window.addEventListener("theme-changed", (e) => {
 });
 </script>
 <template>
-  <div
-    class="bg-background-color text-text-color"
-    :class="{
-      'theme-dark': theme == 'dark',
-      'theme-light': theme == 'light',
-      'theme-blue': theme == 'blue',
-    }"
-  >
+  <div class="bg-background-color text-text-color" :class="{
+    'theme-dark': theme == 'dark',
+    'theme-light': theme == 'light',
+    'theme-blue': theme == 'blue',
+  }">
     <TitleBar />
     <NavBar />
     <RouterView />
