e66faed66ee0670f9dcaecc6346a704602f05baa
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-29 22:10:59 +0800

    MTDSIM-96 full re-work of settings page, added in dif colors, add in images as well as new component

--- client/src/components/ThemeCard.vue
+++ client/src/components/ThemeCard.vue
@@ -1,17 +1,36 @@
 <template>
-    <div class="bg-gray-500 mx-6 px-16 py-16 rounded-md">
-        <div class="flex flex-row justify-center items-center">
-            <p>PHOTO</p>
-        </div>
-        <div class="bg-gray-200">
-            <p>{{ theme }}</p>
-        </div>
+  <div
+    class="bg-navbar-primary overflow-hidden shadow-lg rounded-md hover:cursor-pointer"
+    @click="toggleTheme(theme)"
+  >
+    <div
+      class="w-48 h-32 bg-background-secondary flex flex-row items-center justify-center"
+    >
+      <img class="h-24 w-32" :src="link" />
     </div>
+    <div class="px-6 py-4 flex flex-row items-center">
+      <input
+        :checked="active"
+        id="default-radio-2"
+        type="radio"
+        name="default-radio"
+        class="w-4 h-4 bg-gray-100 border-gray-300 hover:cursor-pointer"
+      />
+      <label class="hover:cursor-pointer pl-2">{{ theme }}</label>
+    </div>
+  </div>
 </template>
 
 <script setup>
-defineProps({
-    theme: { type: String, default: "" }
+const props = defineProps({
+  theme: { type: String, default: "" },
+  active: { type: Boolean, default: false },
+  link: { type: String, default: "" },
 });
-const emit = defineEmits(["update:theme"]);
+const emit = defineEmits(["update:modelValue"]);
+
+const toggleTheme = (theme) => {
+  console.log(theme);
+  emit("update:modelValue", theme);
+};
 </script>
--- client/src/views/SettingsView.vue
+++ client/src/views/SettingsView.vue
@@ -1,28 +1,44 @@
 <template>
-  <div class="h-[calc(100vh-36px)]">
-    <p class="text-2xl pb-2">Settings page</p>
-    <p class="text-md">Customise your experience, make it more enjoyable and easier to work!</p>
-    <div class="flex flex-col">
-      <div class="flex flex-row">
-        <div v-for="theme in themes" :key="theme">
-          <ThemeCard :theme="theme" />
-        </div>
-        <!-- <button @click="setTheme('dark')">Dark Theme</button>
-      <button @click="setTheme('light')">Light Theme</button> -->
+  <div class="h-[calc(100vh-36px)] flex flex-col pl-8 pt-8">
+    <p class="text-2xl pb-4">Settings page</p>
+    <p class="text-md">
+      Customise your experience, make it more enjoyable and easier to work!
+    </p>
+    <div class="flex flex-row py-4 space-x-5">
+      <div v-for="theme in themes" :key="theme">
+        <ThemeCard
+          :theme="theme.theme"
+          :active="theme.active"
+          :link="theme.link"
+          @update:model-value="setTheme(theme.theme)"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import ThemeCard from '../components/ThemeCard.vue';
+import ThemeCard from "../components/ThemeCard.vue";
+import { ref } from "vue";
+import dark from "../public/screens/Dark.png";
+import light from "../public/screens/Light.png";
 const setTheme = (theme) => {
-  localStorage.setItem("user-theme", theme);
+  localStorage.setItem("user-theme", theme.toLowerCase());
   window.dispatchEvent(
     new CustomEvent("theme-changed", {
       detail: { storage: localStorage.getItem("user-theme") },
     }),
   );
+  themes.value.forEach((presetTheme) => {
+    if (presetTheme.theme === theme) {
+      presetTheme.active = true;
+    } else {
+      presetTheme.active = false;
+    }
+  });
 };
-const themes = ["dark", "light"];
+const themes = ref([
+  { theme: "Dark", link: dark, active: true },
+  { theme: "Light", link: light, active: false },
+]);
 </script>
