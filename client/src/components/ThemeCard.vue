6dace4144b7ecdaffa752a919ebd209d2d2c3a68
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-29 17:52:46 +0800

    MTDSIM-96 start implementation

---
+++ client/src/components/ThemeCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-gray-500 mx-6 px-16 py-16 rounded-md">
+        <div class="flex flex-row justify-center items-center">
+            <p>PHOTO</p>
+        </div>
+        <div class="bg-gray-200">
+            <p>{{ theme }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    theme: { type: String, default: "" }
+});
+const emit = defineEmits(["update:theme"]);
+</script>
--- client/src/views/SettingsView.vue
+++ client/src/views/SettingsView.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="flex flex-col items-center h-[calc(100vh-36px)]">
-    <h1>Settings page</h1>
-    <h2>Theme Variants:</h2>
-    <button @click="setTheme('dark')">Dark Theme</button>
-    <button @click="setTheme('light')">Light Theme</button>
+  <div class="h-[calc(100vh-36px)]">
+    <p class="text-2xl pb-2">Settings page</p>
+    <p class="text-md">Customise your experience, make it more enjoyable and easier to work!</p>
+    <div class="flex flex-col">
+      <div class="flex flex-row">
+        <div v-for="theme in themes" :key="theme">
+          <ThemeCard :theme="theme" />
+        </div>
+        <!-- <button @click="setTheme('dark')">Dark Theme</button>
+      <button @click="setTheme('light')">Light Theme</button> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import ThemeCard from '../components/ThemeCard.vue';
 const setTheme = (theme) => {
   localStorage.setItem("user-theme", theme);
   window.dispatchEvent(
@@ -16,4 +24,5 @@ const setTheme = (theme) => {
     }),
   );
 };
+const themes = ["dark", "light"];
 </script>
