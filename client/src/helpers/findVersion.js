63f13a7d1b70164e828229695c8ade5728939b84
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-29 17:02:26 +0800

    MTDSIM-115 update if electron helper

--- client/src/helpers/findVersion.js
+++ client/src/helpers/findVersion.js
@@ -1,11 +1,11 @@
 /**
- * Takes in the window element from the 'website' and tests it to see if its an app or not
- * @param {window} window element
+ * Takes in the user agent element from the 'website' and tests it to see if its an app or not
+ * @param {navigator.userAgent} agent element
  * @returns {boolean} true if electron, false if not
  */
 
-export default function findVersion(window) {
-  if ((window && window.process && window.process.type == "renderer") == true)
+export default function findVersion(userAgent) {
+  if (userAgent.indexOf("Electron") > -1)
     return true;
   else return false;
 }
