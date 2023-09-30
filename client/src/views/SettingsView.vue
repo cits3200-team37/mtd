d2c47aa9a3170f30ffc834b234a331893b0993d1
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-30 11:07:53 +0800

    MTDSIM-96 prepare for update functionality

--- client/src/views/SettingsView.vue
+++ client/src/views/SettingsView.vue
@@ -6,13 +6,14 @@
     </p>
     <div class="flex flex-row py-4 space-x-5">
       <div v-for="theme in themes" :key="theme">
-        <ThemeCard
-          :theme="theme.theme"
-          :active="theme.active"
-          :link="theme.link"
-          @update:model-value="setTheme(theme.theme)"
-        />
+        <ThemeCard :theme="theme.theme" :active="theme.active" :link="theme.link"
+          @update:model-value="setTheme(theme.theme)" />
       </div>
+    </div>
+    <div>
+      <p class="text-2xl pb-2">Current Version</p>
+      <p class="text-md pb-4">{{ version }}</p>
+      <button class="bg-navbar-primary rounded-md px-2 py-2" @click="checkUpdate">Check for Updates</button>
     </div>
   </div>
 </template>
@@ -22,11 +23,12 @@ import ThemeCard from "../components/ThemeCard.vue";
 import { ref, onMounted } from "vue";
 import dark from "../public/screens/Dark.png";
 import light from "../public/screens/Light.png";
+const version = ref();
 const currentTheme = ref();
 onMounted(async () => {
   try {
     currentTheme.value = localStorage.getItem("user-theme");
-    console.log(currentTheme.value);
+    console.log(version.value);
     await setTheme(currentTheme.value);
   } catch (e) {
     console.log(e);
@@ -52,4 +54,9 @@ const themes = ref([
   { theme: "Dark", link: dark, active: false },
   { theme: "Light", link: light, active: false },
 ]);
+
+const checkUpdate = () => {
+  console.log("Checking for updates...");
+}
+
 </script>
