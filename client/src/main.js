3e61f6f01cbc57f9cbf1b5b23274adb414ce812a
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-30 11:31:39 +0800

    MTDSIM-96 remove electron-log

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,7 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 const path = require("path");
 require("update-electron-app")({
-  updateInterval: "1 hour",
-  logger: require("electron-log"),
+  updateInterval: "1 hour"
 });
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
