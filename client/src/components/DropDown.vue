93dacd056cd5b379e9e77a489598b03b95cf51f0
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2023-10-01 13:42:19 +0800

    Format

--- client/src/components/DropDown.vue
+++ client/src/components/DropDown.vue
@@ -1,7 +1,9 @@
 <template>
   <label>{{ label }}</label>
-  <div class="p-1 mt-2 pl-2.5 border border-solid rounded-md text-black w-full bg-white hover:cursor-pointer"
-    :class="{ 'mb-2.5': !isOpen, 'border-red-500 border-4': error }">
+  <div
+    class="p-1 mt-2 pl-2.5 border border-solid rounded-md text-black w-full bg-white hover:cursor-pointer"
+    :class="{ 'mb-2.5': !isOpen, 'border-red-500 border-4': error }"
+  >
     <div class="flex items-center" @click="isOpen = !isOpen">
       <div v-if="!selected">
         <span class="text-gray-400">
@@ -14,18 +16,31 @@
         </span>
       </div>
       <div class="ml-auto">
-        <svg-icon type="mdi" size="20" :path="mdiChevronDown" :class="{ 'rotate-180': isOpen }"></svg-icon>
+        <svg-icon
+          type="mdi"
+          size="20"
+          :path="mdiChevronDown"
+          :class="{ 'rotate-180': isOpen }"
+        ></svg-icon>
       </div>
     </div>
   </div>
-  <div v-if="isOpen" class="z-10 rounded-md bg-white shadow-md w-full overflow-hidden">
+  <div
+    v-if="isOpen"
+    class="z-10 rounded-md bg-white shadow-md w-full overflow-hidden"
+  >
     <ul>
-      <li v-for="(item, index) in menuOptions" :key="index"
+      <li
+        v-for="(item, index) in menuOptions"
+        :key="index"
         class="px-4 py-2 text-sm text-gray-500 leading-5 hover:bg-slate-300 hover:text-black focus:outline-none focus:bg-gray-50 hover:cursor-pointer"
-        @click="handleClick(item)" :class="{
-          'hover:cursor-not-allowed text-gray-500 hover:text-black': item != 'Random',
-          'bg-slate-300': item === selected
-        }">
+        @click="handleClick(item)"
+        :class="{
+          'hover:cursor-not-allowed text-gray-500 hover:text-black':
+            item != 'Random',
+          'bg-slate-300': item === selected,
+        }"
+      >
         {{ item }}
       </li>
     </ul>
@@ -56,7 +71,7 @@ const selected = ref(props.modelValue);
 
 watch(
   () => props.modelValue,
-  (val) => (selected.value = val),
+  (val) => (selected.value = val)
 );
 
 const handleClick = (item) => {
