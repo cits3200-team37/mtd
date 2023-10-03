5d61e629a028ba63720a21681faae7730acecd33
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2023-10-03 14:24:43 +0800

    Replace "None" in schemes

--- client/src/stores/simulation.js
+++ client/src/stores/simulation.js
@@ -13,9 +13,8 @@ export const useSimulationStore = defineStore("simulation", {
     async simulate(simulateFormValues) {
       this.parameters = { ...simulateFormValues };
       const reqBody = {
+        networkSizeList: Number(this.parameters.networkSizeList),
         scheme: this.parameters.scheme,
-        mtdInterval: Number(this.parameters.mtdInterval),
-        finishTime: Number(this.parameters.finishTime),
         totalNodes: Number(this.parameters.totalNodes),
       };
 
