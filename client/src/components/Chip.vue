69d4b17b6757f1fc937a75a22df376c72cb78932
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2023-10-09 13:55:11 +0800

    Chip content centered and fixed

--- client/src/components/Chip.vue
+++ client/src/components/Chip.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="inline-flex text-white rounded-full bg-chip-color px-2 py-px justify-center content-center cursor-default"
+    class="inline-flex  text-white rounded-full bg-chip-color text-center justify-center content-center cursor-default py-1 px-3 my-0.5"
   >
-    <span class="truncate block max-w-xs align-middle text-xs mr-2">{{
+    <span class="truncate block max-w-xs align-middle text-xs mr-2 justify-self-center self-center">{{
       label
     }}</span>
     <div class="cursor-pointer">
