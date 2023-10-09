93fd714c270cde4b7220e87bd6fbad193abc4202
FearMyCode <mcalyip@gmail.com>
2023-10-09 20:07:25 +0800

    MTDSIM-103 text wrapping

--- client/src/components/Chip.vue
+++ client/src/components/Chip.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="inline-flex text-white rounded-full bg-sub-color text-center justify-center content-center cursor-default py-1 px-3 my-0.5"
+    class="inline-flex items-center text-white rounded-full bg-sub-color text-center justify-center content-center cursor-default py-1 px-3 my-0.5"
   >
     <span
-      class="truncate block max-w-xs align-middle text-xs mr-2 justify-self-center self-center"
+      class="block max-w-xs align-middle text-xs mr-2 justify-self-center self-center"
       >{{ label }}</span
     >
     <div class="cursor-pointer">
